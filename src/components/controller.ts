6dcd2c491e381d14f0d2e0fd865b3fa9a3e00c83
Vlad <makrak.vladislav@yandex.ru>
2023-01-16 18:28:28 +0300

    feat: add controller methods

--- src/components/controller.ts
+++ src/components/controller.ts
@@ -1,13 +1,25 @@
+import IUser from 'src';
 import { v4 as uuidv4 } from 'uuid';
 
 class Controller {
-  users: any = [];
+  users: Array<IUser> = [];
 
   async getUsers() {
     return this.users;
   }
 
-  async createUser(userData: any) {
+  async getById(userId: string) {
+    return new Promise((resolve, reject) => {
+      const userById = this.users.filter((user: { id: string }) => user.id === userId);
+      if (userById.length !== 0) {
+        resolve(userById);
+      } else {
+        reject(`User not found by id ${userId}`);
+      }
+    });
+  }
+
+  async createUser(userData: Omit<IUser, 'id'>) {
     return new Promise((resolve) => {
       const user = {
         id: uuidv4(),
@@ -15,6 +27,33 @@ class Controller {
       };
       this.users.push(user);
       resolve(user);
+    });
+  }
+
+  async updateUser(userId: string, userData: Omit<IUser, 'id'>) {
+    return new Promise((resolve, reject) => {
+      const userIndex = this.users.findIndex((user) => user.id === userId);
+      if (userIndex !== -1) {
+        this.users[userIndex] = {
+          id: userId,
+          ...userData,
+        };
+        resolve(this.users[userIndex]);
+      } else {
+        reject(`User not found by id ${userId}`);
+      }
+    });
+  }
+
+  async deleteUser(userId: string) {
+    return new Promise((resolve, reject) => {
+      const userIndex = this.users.findIndex((user) => user.id === userId);
+      if (userIndex !== -1) {
+        this.users.splice(userIndex, 1);
+        resolve(`User ${userId} deleted`);
+      } else {
+        reject(`User not found by id ${userId}`);
+      }
     });
   }
 }
